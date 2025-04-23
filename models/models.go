21fb89a1a50b3615120296086655f3e3762e9199
Mayuresh Anand <56501614+mayureshucsb2019@users.noreply.github.com>
2025-04-22 21:55:25 -0400

    add new eoute for generateKCV and models

--- models/models.go
+++ models/models.go
@@ -3,3 +3,16 @@ package models
 type GenerateKeyResp struct {
 	Key string `json:"Key"`
 }
+
+type GenerateKeyReq struct {
+	Size int    `json:"Size"`
+	PRNG string `json:"PRNG"`
+}
+
+type GenerateKCVReq struct {
+	Key string `json:"Key"`
+}
+
+type GenerateKCVResp struct {
+	KCV string `json:"KCV"`
+}
--- router/crypto.go
+++ router/crypto.go
@@ -13,6 +13,7 @@ func CryptoRoutes(r *mux.Router, logger *logrus.Logger) {
 	cs := services.NewCryptoService(logger)
 	ch := handler.NewCryptoHandler(logger, cs)
 
-	r.HandleFunc("/generateKey", ch.GenerateKey).Methods("GET")
+	r.HandleFunc("/generateKey", ch.GenerateKey).Methods("POST")
+	r.HandleFunc("/generateKCV", ch.GenerateKCV).Methods("POST")
 
 }
